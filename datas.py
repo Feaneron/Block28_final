a193ecf8baf6fc78d7a9be16487425ed1bb55f0d
Герасименко И. Qap1013 <feaneron8854@gmail.com>
2023-07-10 20:55:15 +0300

    Initial commit

---
+++ datas.py
@@ -0,0 +1,36 @@
+# Данные для входа
+valid_phone = 79889945749
+empty_phone = ""
+wronge_phone = 7988994574
+
+valid_mail = "feaneron@rambler.ru"
+empty_mail = " "
+wronge_mail = "jfer@vasege.com"
+
+valid_login = "feaneron"
+empty_login = ""
+wronge_login = "jshdbf"
+
+valid_auth_number = 461008845619
+worded_auth_number = "46100vv45619"
+wronge_auth_number = 461008845618
+
+valid_passwrd = "82Om@nag"
+wronge_passwrd = "82Om@svbgearth"
+empty_passwrd = ""
+
+# Данные регистрации
+
+name = "Иван"
+wronge_name = "Сеня"
+empty_name = ""
+
+last_name = "Герасименко"
+wronge_last_name = "Пупкин"
+empty_last_name = ""
+
+
+# Урлы
+normal_url_id = "https://b2c.passport.rt.ru/account_b2c/page?state=3af7c308-e98b-4661-a0a3-60bba3bea906&client_id=account_b2c#/"
+url_rtk = "https://b2c.passport.rt.ru"
+url_chromedriver = r"F:\Programs-project\final_project\tests"
